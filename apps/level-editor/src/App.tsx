f9a81213024214130d33203f7f4644a86d82fd85
Clinton Selke <clinuxrulz@gmail.com>
2026-06-05 10:32:30 +1000

    preparing level editor app

---
+++ apps/level-editor/src/App.tsx
@@ -0,0 +1,7 @@
+import { Component } from "solid-js";
+
+const App: Component = () => {
+  return <>Test</>;
+};
+
+export default App;
---
+++ apps/level-editor/src/index.tsx
@@ -0,0 +1,5 @@
+import { render } from "@solidjs/web";
+import App from "./App";
+
+const root = document.getElementById("root");
+if (root) render(() => <App/>, root);
